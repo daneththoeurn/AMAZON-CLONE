dd703eed20c4185fe6edbb14c4ba5201d3741be8
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-08-31 00:26:34 +0700

    added code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build the Amazon Store!</h1>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1qd_KxdXCxDIV5h3k8CaFa1vbl1kU51o",
+    authDomain: "clone-337e1.firebaseapp.com",
+    projectId: "clone-337e1",
+    storageBucket: "clone-337e1.appspot.com",
+    messagingSenderId: "786787054934",
+    appId: "1:786787054934:web:abdaed2291af33ea3c92f7",
+    measurementId: "G-H9CXNFL40C"
+  };
