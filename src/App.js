e72a9cf984d70c54e82836c3fb14c2bd36236789
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-08-31 01:31:10 +0700

    added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the Amazon Store!</h1>
+      <Header />
       {/* Header */}
       {/* Home */}
     </div>
