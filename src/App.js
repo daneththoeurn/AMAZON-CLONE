a5e5ccad92b6e9c20598d74787318f958e581601
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-09-01 00:47:37 +0700

    added Home Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Header */}
-      {/* Home */}
+      <Home />
     </div>
   );
 }
