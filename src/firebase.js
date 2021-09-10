590fc1298b427d60b9e0bd1997b49d6c810d715f
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-09-10 17:10:45 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,17 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC1qd_KxdXCxDIV5h3k8CaFa1vbl1kU51o",
-    authDomain: "clone-337e1.firebaseapp.com",
-    projectId: "clone-337e1",
-    storageBucket: "clone-337e1.appspot.com",
-    messagingSenderId: "786787054934",
-    appId: "1:786787054934:web:abdaed2291af33ea3c92f7",
-    measurementId: "G-H9CXNFL40C"
-  };
+  apiKey: "AIzaSyC1qd_KxdXCxDIV5h3k8CaFa1vbl1kU51o",
+  authDomain: "clone-337e1.firebaseapp.com",
+  projectId: "clone-337e1",
+  storageBucket: "clone-337e1.appspot.com",
+  messagingSenderId: "786787054934",
+  appId: "1:786787054934:web:abdaed2291af33ea3c92f7",
+  measurementId: "G-H9CXNFL40C",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
